f221da909f55a0ed1f0cedd1c3c835706f083af1
Katya Sheps <katya@sheps.ru>
2019-07-06 22:43:41 +0300

    Fixed bugs

--- app/src/main/java/ru/sheps/android/homework721/MainActivity.java
+++ app/src/main/java/ru/sheps/android/homework721/MainActivity.java
@@ -6,27 +6,46 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText editCoordinate;
-    EditText editAddress;
+    EditText edit;
+    private String input;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editCoordinate = findViewById(R.id.coordinate);
-        editAddress = findViewById(R.id.address);
+        edit = findViewById(R.id.edit);
 
     }
 
     public void onClick(View view) {
-       Intent intent = new Intent(Intent.ACTION_VIEW);
-      //  if (editAddress.getText().toString().equals("") | editCoordinate.getText().toString().equals("")) {
-        Uri uri = Uri.parse("geo:?q=Moscow");
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        input = edit.getText().toString();
+        if (input.equals("")) {
+            Toast.makeText(this, "Заполните пустое поле ввода текста", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        char[] charArray = input.toCharArray();
+
+        boolean hasLetter = false;
+        for (char ch : charArray) {
+            if (Character.isLetter(ch)) {
+                hasLetter = true;
+                break;
+            }
+        }
+        if (hasLetter) {
+            input = "geo:?q=" + input;
+        } else {
+            input = "geo:" + input;
+        }
+
+        Uri uri = Uri.parse(input);
         intent.setData(uri);
         startActivity(intent);
-       // }
     }
 }
